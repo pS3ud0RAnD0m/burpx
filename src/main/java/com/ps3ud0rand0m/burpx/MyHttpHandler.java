a88be05d6ff782e92b95ce101b7def15eb7d5d0e
p$3ud0R@nD0m <joshualewishyde@gmail.com>
2023-11-24 17:27:30 -0500

    Added MyHttpHandler class

---
+++ src/main/java/com/ps3ud0rand0m/burpx/MyHttpHandler.java
@@ -0,0 +1,57 @@
+package com.ps3ud0rand0m.burpx;
+
+import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.core.Annotations;
+import burp.api.montoya.core.HighlightColor;
+import burp.api.montoya.http.handler.*;
+import burp.api.montoya.http.message.requests.HttpRequest;
+import burp.api.montoya.logging.Logging;
+
+import static burp.api.montoya.http.handler.RequestToBeSentAction.continueWith;
+import static burp.api.montoya.http.handler.ResponseReceivedAction.continueWith;
+import static burp.api.montoya.http.message.params.HttpParameter.urlParameter;
+
+class MyHttpHandler implements HttpHandler {
+    private final Logging logging;
+
+    public MyHttpHandler(MontoyaApi api) {
+        this.logging = api.logging();
+    }
+
+
+    @Override
+    public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent requestToBeSent) {
+        Annotations annotations = requestToBeSent.annotations();
+
+        // If the request is a post, log the body and add notes.
+        //if (isPost(requestToBeSent)) {
+        //    annotations = annotations.withNotes("Request was a post");
+        //    logging.logToOutput(requestToBeSent.bodyToString());
+        //}
+
+        // Modify the request by adding url param.
+        HttpRequest modifiedRequest = requestToBeSent.withAddedParameters(urlParameter("added", "parameter"));
+
+        // Return the modified request to burp with updated annotations.
+        return continueWith(modifiedRequest, annotations);
+    }
+
+    @Override
+    public ResponseReceivedAction handleHttpResponseReceived(HttpResponseReceived responseReceived) {
+        Annotations annotations = responseReceived.annotations();
+        // Highlight all responses where the request had a Content-Length header.
+        if (responseHasContentLengthHeader(responseReceived)) {
+            annotations = annotations.withHighlightColor(HighlightColor.RED);
+        }
+
+        return continueWith(responseReceived, annotations);
+    }
+
+    private static boolean isPost(HttpRequestToBeSent httpRequestToBeSent) {
+        return httpRequestToBeSent.method().equalsIgnoreCase("POST");
+    }
+
+    private static boolean responseHasContentLengthHeader(HttpResponseReceived httpResponseReceived) {
+        return httpResponseReceived.initiatingRequest().headers().stream().anyMatch(header -> header.name().equalsIgnoreCase("Content-Length"));
+    }
+}
