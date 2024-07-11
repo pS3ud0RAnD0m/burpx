c5d7e70edb64b2a78d95ac527d827701e6bb025d
pr <pr@ps3ud0rand0m.com>
2024-07-11 10:20:48 -0400

    ..

--- src/main/java/com/ps3ud0rand0m/burpx/Burpx.java
+++ src/main/java/com/ps3ud0rand0m/burpx/Burpx.java
@@ -3,19 +3,18 @@ package com.ps3ud0rand0m.burpx;
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 
-public class Burpx implements BurpExtension
-{
+public class Burpx implements BurpExtension {
     @Override
-    public void initialize(MontoyaApi api)
-    {
+    public void initialize(MontoyaApi api) {
         api.extension().setName("Burpx");
 
         // Http handlers
-        api.http().registerHttpHandler(new MyHttpHandler(api));
+        //api.http().registerHttpHandler(new MyHttpHandler(api));
         //api.http().registerHttpHandler(new MyHttpHandlerTinker(api));
 
         // Proxy handlers
-        //api.proxy().registerRequestHandler(new MyProxyRequestHandler());
+        api.proxy().registerRequestHandler(new MyProxyRequestHandler(api));
+        //api.proxy().registerRequestHandler(new MyProxyRequestHandlerTinker());
         //api.proxy().registerResponseHandler(new MyProxyResponseHandler());
     }
 }
--- src/main/java/com/ps3ud0rand0m/burpx/MyProxyRequestHandler.java
+++ src/main/java/com/ps3ud0rand0m/burpx/MyProxyRequestHandler.java
@@ -1,29 +1,45 @@
 package com.ps3ud0rand0m.burpx;
 
+import burp.api.montoya.logging.Logging;
 import burp.api.montoya.proxy.http.InterceptedRequest;
 import burp.api.montoya.proxy.http.ProxyRequestHandler;
 import burp.api.montoya.proxy.http.ProxyRequestReceivedAction;
 import burp.api.montoya.proxy.http.ProxyRequestToBeSentAction;
+import burp.api.montoya.MontoyaApi;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static burp.api.montoya.core.HighlightColor.RED;
 import static burp.api.montoya.http.message.ContentType.JSON;
 
 class MyProxyRequestHandler implements ProxyRequestHandler {
+
+    private final Logging logging;
+
+    public MyProxyRequestHandler(MontoyaApi api) {
+        this.logging = api.logging();
+    }
+
     @Override
     public ProxyRequestReceivedAction handleRequestReceived(InterceptedRequest interceptedRequest) {
+        // Print the intercepted request URL to standard output for debugging
+        //logging.logToOutput("Intercepted URL: " + interceptedRequest.url());
+
         // Drop all post requests
         //if (interceptedRequest.method().equals("POST")) {
         //    return ProxyRequestReceivedAction.drop();
         //}
 
-        // Drop requests with <string> in the url
-        if (interceptedRequest.url().contains("/api/stats/automatic/")) {
+        // Drop requests with any string in the dropUrls list in the url
+        if (shouldDropRequest(interceptedRequest.url())) {
             return ProxyRequestReceivedAction.drop();
         }
 
-        // Intercept requests with <string> in the url
-        //if (interceptedRequest.url().contains("foo")) {
-        //    return ProxyRequestReceivedAction.intercept(interceptedRequest);
+        // Drop requests with <string> in the url
+        //if (interceptedRequest.url().contains("mydroptest")) {
+        //    logging.logToOutput("Dropping URL: " + interceptedRequest.url());
+        //    return ProxyRequestReceivedAction.drop();
         //}
 
         // If the content type is json, highlight the request and follow burp rules for interception
@@ -38,8 +54,30 @@ class MyProxyRequestHandler implements ProxyRequestHandler {
         return ProxyRequestReceivedAction.continueWith(interceptedRequest);
     }
 
+    private boolean shouldDropRequest(String url) {
+        // Hard-coded list of strings to drop
+        List<String> dropUrls = Arrays.asList(
+                "mydroptest",
+                "analytics.com/",
+                "doubleclick.net/",
+                "drift.com/",
+                "fullstory.com/",
+                "analytics.google.com/",
+                "telemetry.elastic.co/"
+        );
+
+        for (String dropUrl : dropUrls) {
+            if (url.contains(dropUrl)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Override
     public ProxyRequestToBeSentAction handleRequestToBeSent(InterceptedRequest interceptedRequest) {
+        // Do nothing with the user modified request, continue as normal.
         return ProxyRequestToBeSentAction.continueWith(interceptedRequest);
     }
 }
